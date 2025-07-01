111f70092cd9d20463ce459d64f0f0c334510a1a
Sanya Sharma <sanya.sharma1301@gmail.com>
2025-07-01 22:38:19 +0530

    Create MatrixAddition.java

---
+++ Matrix/MatrixAddition.java
@@ -0,0 +1,52 @@
+
+// Program to add two matrices of the same size
+
+import java.util.*;
+
+public class MatrixAddition {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input dimensions
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] A = new int[rows][cols];
+        int[][] B = new int[rows][cols];
+        int[][] sum = new int[rows][cols];
+
+        // Input first matrix
+        System.out.println("Enter elements of first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        // Input second matrix
+        System.out.println("Enter elements of second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        //Add the matrices
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sum[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        // Print result
+        System.out.println("Resultant Matrix (Sum):");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
