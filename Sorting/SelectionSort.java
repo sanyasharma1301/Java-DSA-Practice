a1738211cd08eb313d61f31580f1736deae1b9b3
Sanya Sharma <sanya.sharma1301@gmail.com>
2025-07-01 22:28:01 +0530

    Create SelectionSort.java

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,45 @@
+
+// Program to sort an array using selection sort algorithm
+
+import java.util.*;
+
+public class SelectionSort {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        // Input elements
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Selection sort logic
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find index of minimum element
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        // Output sorted array
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
